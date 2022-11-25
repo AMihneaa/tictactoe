2438a2be25b075d54cff6cf3682ae86e7ff1db49
Mihnea <asmihnea5@gmail.com>
2022-11-26 00:35:38 +0200

    calculate the winner

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function Square({ value, onClick }) {
-  console.log(value);
-
   return (
     <button type="button" className="square" onClick={onClick}>
       {value}
